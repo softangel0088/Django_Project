0b5e08f5336b9f050b3165d94169dd52b1a52a1f
lionking-123 <69095724+lionking-123@users.noreply.github.com>
2021-07-21 12:33:06 +0300

    model create & migrate
    Model creation and Migrate

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class News(models.Model):
+    author = models.CharField(max_length=30)
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+
+
+class SportNews(models.Model):
+    author = models.CharField(max_length=30)
+    title = models.CharField(max_length=30)
+    description = models.TextField()

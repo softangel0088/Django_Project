f79a79dd001d7b9d265deba5679310d256c5e380
lionking-123 <69095724+lionking-123@users.noreply.github.com>
2021-07-21 12:22:43 +0300

    initial commit
    Initial commit

---
+++ dashboard/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    text = """<h1>Welcome to my app!</h1>"""
+    return HttpResponse(text)
